30578929d5313ccab0b8ea56cb339bd95cf92d8d
argentonyco <67129387+argentonyco@users.noreply.github.com>
2022-02-23 11:35:25 -0300

    23-02-22

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -12,7 +12,7 @@
 # Pedir por consola dos números que representen el principio y fin de una
 # secuencia numérica.
 # Realizar un bucle "for" que recorra esa secuencia armada con "range"
-# y calcule a sumatoria total de todos los números dentro de esa secuencia
+# y calcule la sumatoria total de todos los números dentro de esa secuencia
 # Tener en cuenta que "range" no incluye el número de "fin" en su secuencia,
 # sino que va hasta el anterior
 
@@ -20,43 +20,16 @@ inicio = int(input('Ingrese el primer número de la secuencia\n'))
 fin = int(input('Ingrese el último número de la secuencia\n'))
 sumatoria = 0  # Inicializo el contador en 0
 
-# for ... in range(....)
+if fin < inicio:
+    for numeros in range(fin, inicio + 1):
+        sumatoria = sumatoria + numeros
+    # Imprimir el valor de la sumatoria
+        
+else:
+    for numeros in range(inicio, fin + 1):
+        sumatoria = sumatoria + numeros
+    # Imprimir el valor de la sumatoria
+        
 
-# Imprimir el valor de la sumatoria
+print(f'La suma total de todos los numeros de la secuencia es: {sumatoria}')
 
-print("terminamos!")
-
-maximo_numero = None
-lista_numeros = []
-
-while True:
-    # Tomamos el valor de la consola
-    print("Ingrese un número mayor o igual a cero")
-    numero = int(input())
-
-    # Verificamos si el número es negativo
-    if numero < 0:
-        print('Hasta acá llegamos!')
-        break   # Salgo del bucle!
-
-    # Verifico si el número ingresado es mayor al
-    # máximo número ingresado hasta el momento
-    if (maximo_numero is None) or (numero > maximo_numero):
-        maximo_numero = numero
-
-    lista_numeros.append(numero)
-
-# Termino el bucle imprimo la lista de números:
-print("Lista: ", lista_numeros)
-# Imprimo el máximo número encontrado
-print("Máximo número = ", maximo_numero)
-# Imprimo el máximo número utilizando la función max de Python
-print("Máximo número con Python = ", max(lista_numeros))
-# Imprimo el índice del máximo número en la lista
-print("Índice del máximo número en la lista = ",
-        lista_numeros.index(maximo_numero))
-# Imprimo cuantas veces se repite el máximo número en la lista
-print("Cantidad del máximo número en la lista = ",
-        lista_numeros.count(maximo_numero))
-
-print("terminamos!")
--- ejercicios_practica/ejercicio_6.py
+++ ejercicios_practica/ejercicio_6.py
@@ -20,9 +20,34 @@ inicio = int(input('Ingrese el primer número de la secuencia\n'))
 fin = int(input('Ingrese el último número de la secuencia\n'))
 
 cantidad_numeros_positivos = 0  # Inicializo el contador en 0
+cantidad_numeros_negativos = 0
+cantidad_cero = 0
 
-# for ... in range(....)
+if fin < inicio:
+    for i in range(fin, inicio + 1):
+        if i < 0:            
+            cantidad_numeros_negativos += 1  # imprimi numeros < 0
+        elif i > 0:
+            cantidad_numeros_positivos += 1
+        else:
+            cantidad_cero += 1   
+        
+else:
+    for i in range(inicio, fin + 1):
+        if i < 0:            
+            cantidad_numeros_negativos += 1
+        elif i > 0:
+            cantidad_numeros_positivos += 1
+        else:
+            cantidad_cero = cantidad_cero + 1
+
+print(f'La cantidad de numeros negativos de su cadena es: {cantidad_numeros_negativos}')   
+print(f'La cantidad de numeros positivos de su cadena es: {cantidad_numeros_positivos}')   
+print(f'La cantidad de ceros de su cadena es: {cantidad_cero}')   
+
 
 # Imprimir el valor de la cantidad de números positivos y negativos
 
 print("terminamos!")
+
+
